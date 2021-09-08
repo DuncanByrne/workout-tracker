457e7b45a526a2cfbb4ee57dc82b4068ec47c310
Duncan Byrne <duncanbyrne@Duncans-MacBook-Pro.local>
2021-09-07 22:52:17 -0400

    added folders for models and routes

---
+++ models/workout.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+
+  day:{
+    type: Date,
+    default: Date.now
+  },
+  excercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: true
+      },
+      name:{
+        type: String,
+        required: true
+      },
+
+      duration:{
+        type: Number,
+        required: true
+      },
+      distance:{
+        type: Number
+      },
+      weight:{
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+      sets:{
+        type: Number
+      }
+    },
+  ]
+
+},{ toJSON: { virtuals: true} });
+
+WorkoutSchema.virtuals("totalDuration").weight(function() {
+  return this.excercises.reduce((total, excercise) =>{
+    return total + excercise.duration
+  }, 0);
+})
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
